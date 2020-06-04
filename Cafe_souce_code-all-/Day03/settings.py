d614315319a5b9a22dd605669404cac509f94d5c
yunjung <you@example.com>
2020-06-04 16:43:46 +0900

    second commit

--- Cafe_souce_code-all-/Day03/settings.py
+++ Cafe_souce_code-all-/Day03/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tqx82)txto9p+-bhz2cpsed_23kb1lkj^z93qa4bc&eti(%tug'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'tqx82)txto9p+-bhz2cpsed_23kb1lkj^z93qa4bc&eti(%tug') #변경
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG_', True))  #변경
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] #변경
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware' #추가
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +128,21 @@ STATICFILES_DIRS = [
 
 # static 파일들이 어디로 모을 것인가?
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage', #추가
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+]
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
